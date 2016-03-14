61dc822aa9348053e16bb2a78c2f8d08ee9f57b0
Petko Yotov <5ko@5ko.fr>
2016-03-14 08:58:12 +0100

    Add Dr Toolbox Worldwide license

--- WebViewTests/WebViewTests.m
+++ WebViewTests/WebViewTests.m
@@ -6,6 +6,11 @@
 //  Created by Petko Yotov on August 10, 2015.
 //  Copyright (c) 2015-2016 Petko Yotov. All rights reserved.
 //
+//  Commissioned by and developed with Dr Toolbox who are granted
+//  worldwide, perpetual, irrevocable, royalty-free permission,
+//  to use, modify, copy, publish, sub-licence and sell their copies
+//  of the software as long as their “open source licences” are respected
+//
 
 
 #import <XCTest/XCTest.h>
