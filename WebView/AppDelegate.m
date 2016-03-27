10cb83f19294f070dbfdf6ec1bc3c6675b072631
Petko Yotov <5ko@5ko.fr>
2016-03-27 14:50:03 +0200

    Fix light text status bar for iPhone 6s, 5s

--- WebView/AppDelegate.m
+++ WebView/AppDelegate.m
@@ -21,7 +21,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  //[[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
+  [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
     // Override point for customization after application launch.
   return YES;
 }
